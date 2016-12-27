d82502bd96dbf7034b888cae93063bc435af7655
Monder <monder_1993@hotmail.com>
2016-12-27 16:11:25 +0200

    prepared the login screen

--- src/LogInPage.java
+++ src/LogInPage.java
@@ -1,10 +1,15 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
 
 public class LogInPage {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -36,6 +41,27 @@ public class LogInPage {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 565, 398);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(217, 107, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(217, 163, 86, 20);
+		frame.getContentPane().add(passwordField);
+		
+		JLabel lblNewLabel = new JLabel("UserName");
+		lblNewLabel.setBounds(53, 107, 86, 20);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		lblNewLabel_1.setBounds(53, 163, 86, 20);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblLoginPage = new JLabel("Log In Page");
+		lblLoginPage.setBounds(214, 30, 128, 62);
+		frame.getContentPane().add(lblLoginPage);
 	}
-
 }
