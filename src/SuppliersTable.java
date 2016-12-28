4047fb8a8de108b6492b895319b7ea0303f151ff
HosamKhatib <hosam.kh@live.com>
2016-12-28 23:07:36 +0200

    building suppliers and suppliers table

---
+++ src/SuppliersTable.java
@@ -0,0 +1,49 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+
+public class SuppliersTable {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SuppliersTable window = new SuppliersTable();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SuppliersTable() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 595, 391);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		table = new JTable();
+		table.setBounds(0, 0, 579, 352);
+		frame.getContentPane().add(table);
+	}
+
+}
